7cec695c890259e7afcf6f1dc3f70e5865f28b99
= <veerendragoudatp40@gmail.com>
2023-08-16 19:51:26 +0530

    second problem

---
+++ hol2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	while(1);
+
+}
+// compile the program by gcc hol2.c
+// execute the program by ./a.out
+// stop the process by ctrl+z
+// send the process to background by command bg
+// check the process information running in the background in the proc directory
+// kill the process.
